3724d9673134de193e971a991b14171b9d74ba09
駿河大樹 <surugatomoki@surugadaikinoMacBook-Pro.local>
2021-02-06 13:47:49 +0900

    try import

--- go/main.go
+++ go/main.go
@@ -1,22 +1,10 @@
-package main
-import (
-	"github.com/labstack/echo"
-	"go-ca-webapi/surunavi/go/driver"
-	"log"
-	"os"
-	"github.com/jinzhu/gorm"
-)
-func main() {
-	dbConn, closeFunc, err := driver.NewDBConnection(
-		os.Getenv("PRJ_GROUP_USERNAME"),
-		os.Getenv("PRJ_GROUP_PASSWORD"),
-		os.Getenv("PRJ_GROUP_INSTANCE"),
-		os.Getenv("PRJ_GROUP_DBNAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer closeFunc()
 
-	app := Initialize(dbConn, echo.New())
-	app.Start()
+package main
+
+import (
+	"surunavi/go/pkg/external"
+)
+
+func main() {
+	external.Router.Run()
 }
